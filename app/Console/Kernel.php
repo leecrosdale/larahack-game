ba57730293219594330c2d6d4ad40ac4e12583a8
Lee Crosdale <accounts@leecrosdale.com>
2018-02-05 08:10:57 +0000

    Issue where game logic wasn't running at correct time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('game:run')->hourly();
+         $schedule->command('game:run')->everyFiveMinutes();
     }
 
     /**
